05964a28ebd55e0e73399b2d408d4b75770942e7
SmallNovice <691513006@qq.com>
2021-12-19 19:23:27 +0800

    fix: strong parameters

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::ProjectsController < ApplicationController
+  before_action :find_project, only: [:show, :update, :destroy]
+
   def index
     @projects = Project.all
     fields =
@@ -9,49 +11,47 @@ class Api::V1::ProjectsController < ApplicationController
   end
 
   def show
-    @project = Project.find(params[:id])
     field = Fields.build(@project)
-    render json: field
-  end
 
-  def new
-    @project = Project.new
+    render json: field
   end
 
   def create
     @project = Project.new(project_params)
-
     if @project.save
       render json: @project
     else
-      render :new, status: :unprocessable_entity
+      render json: { message: 'unable to create project' }, status: 400
     end
   end
 
-  def edit
-    @project = Project.find(params[:id])
-  end
-
   def update
-    @project = Project.find(params[:id])
-
-    if @project.update(project_params)
-      redirect_to @project
+    if @project
+      @project.update(project_params)
+      render json: { message: 'project successfully updated!' }, status: 200
     else
-      render :edit, status: :unprocessable_entity
+      render json: { message: 'unable to update project' }, status: 400
     end
   end
 
   def destroy
-    @project = Project.find(params[:id])
-    @project.destroy
-
-    redirect_to root_path
+    if @project
+      @project.destroy
+      render json: { message: 'project successfully deleted!' }, status: 200
+    else
+      render json: { message: 'unable to delete project' }, status: 400
+    end
   end
 
   private
 
   def project_params
-    params.require(:project).permit(:title, :body)
+    params.permit(:current_project_name, :project_level, :current_year, :current_number,
+                  :before_last_year, :before_last_year_number, :last_year_number, :last_year_number_number,
+                  :category_id)
+  end
+
+  def find_project
+    @project = Project.find(params[:id])
   end
 end
--- app/models/fields.rb
+++ app/models/fields.rb
@@ -2,15 +2,11 @@ class Fields
   def self.build(project)
     {
       'department' => {
-
-      'department_id' => project.category.department.id,
-      'department_name' => project.category.department.department_name,
-      'current_project_name' => project.category.current_project_name
+        'department_id' => project.category.department.id,
+        'department_name' => project.category.department.department_name
       },
-      'project' => build_project_name(project).merge({}),
-      'quarter_tables' => {
-        'current_year' => project.quarter_tables
-      }
+      'project' => build_project_name(project).merge(other_project_hash(project)),
+      'quarter_tables' => project.quarter_tables
     }
   end
 
@@ -20,21 +16,36 @@ class Fields
     level = project.category.project_level
     case level
     when '1'
-    {
-      'project_name' => project.category.current_project_name
-    }
+      {
+        'current_project_name' => project.current_project_name,
+        'project_name' => project.category.current_project_name
+      }
     when '2'
       {
+        'current_project_name' => project.current_project_name,
         'project_name' => project.category.last_project_name,
         project.category.last_project_name => project.category.current_project_name
       }
     when '3'
       last_project = Project.find_by(current_project_name: project.category.last_project_name)
       {
+        'current_project_name' => project.current_project_name,
         'project_name' => last_project.category.last_project_name,
         last_project.category.last_project_name => last_project.category.current_project_name,
         last_project.category.current_project_name => project.category.current_project_name
       }
     end
+  end
+
+  def self.other_project_hash(project)
+    {
+      'current_year' => project.current_year,
+      'current_number' => project.current_number,
+      'before_last_year' => project.before_last_year,
+      'before_last_year_number' => project.before_last_year_number,
+      'last_year_number' => project.last_year_number,
+      'last_year_number_number' => project.last_year_number_number,
+      'category_id' => project.category_id
+    }
   end
 end
